3a92725a655383204e6678a32d036141b8bc4232
Alexander <grozakm0100@protonmail.com>
2022-07-26 17:57:02 +0530

    final version completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 "use strict";
+
 // defining var and const values
+
 let player1Score = document.querySelector(".player1-score");
 let player2Score = document.querySelector(".player2-score");
 let bothPlayerScore = document.querySelector(".player-score");
@@ -17,15 +19,20 @@ const player2CurrentScore = document.querySelector(
 const rollDice = document.querySelector(".roll-dice");
 const player1Name = document.querySelector(".active-player-name");
 const player2Name = document.querySelector(".unactive-player-name");
+const modalNewGameBtn = document.querySelector(".rick-roll-new-game-btn");
+
 // defining functions for long codes
+
 const playerSScores = function () {
   player1Score.textContent = Number(0);
   player2Score.textContent = Number(0);
 };
+
 const playersCurrentScores = function () {
   player1CurrentScore.textContent = Number(0);
   player2CurrentScore.textContent = Number(0);
 };
+
 const switchPlayer = () => {
   document.querySelector(
     `.player${activePlayer}-current-dice-score-textContent`
@@ -42,6 +49,7 @@ const switchPlayer = () => {
   player2Name.classList.toggle("active-player-name");
   player2Name.classList.toggle("unactive-player-name");
 };
+
 const winningModal = () => {
   const modalContainer = document.querySelector("[data-modal-container]");
 
@@ -51,7 +59,9 @@ const winningModal = () => {
   const openModal = function () {
     modalContainer.classList.add("show");
   };
-
+  modalNewGameBtn.addEventListener("click", () => {
+    closeModal();
+  })
   modalContainer.classList.add("show");
 
   document.querySelector(
@@ -68,12 +78,46 @@ const winningModal = () => {
   });
 };
 
+const resetGameNewGameBtn = function () {
+  playing = true;
+  audioNewGame.currentTime = 0;
+  activePlayer = 1;
+  currentScore = 0;
+  scores = [0, 0];
+  playerSScores();
+  playersCurrentScores();
+  diceImage.classList.remove("show-dice-img");
+
+  document.querySelector(
+    `.player${activePlayer}-current-dice-score-textContent`
+  ).textContent = Number(0);
+
+
+  setTimeout(() => {
+    player1Bg.classList.remove("unactive-color");
+    player1Bg.classList.add("active-color");
+    player2Bg.classList.add("unactive-color");
+    player2Bg.classList.remove("active-color");
+
+    player1Name.classList.add("active-player-name");
+    player1Name.classList.remove("unactive-player-name");
+    player2Name.classList.remove("active-player-name");
+    player2Name.classList.add("unactive-player-name");
+  }, 100);
+};
+const audioAddCode = function () {
+  document.querySelector(".roll-dice").setAttribute("onclick", "audioDiceFall.play()");
+  document.querySelector(".hold").setAttribute("onclick", "audioDiceHold.play()");
+}
+
 // main functuality starts here
+
 playerSScores();
 playersCurrentScores();
 diceImage.classList.add("hide-dice-img");
 
 //roll dice functuality starts here
+
 let currentScore = 0;
 let activePlayer = 1;
 let scores = [0, 0];
@@ -110,6 +154,7 @@ for (let i = 100; i <= 900; i = i + 200) {
 }
 
 //hold dice functuality starts here
+
 hold.addEventListener("click", () => {
   if (playing) {
     audioDiceHold.currentTime = 0.07;
@@ -118,12 +163,12 @@ hold.addEventListener("click", () => {
 
     document.querySelector(`.player${activePlayer}-score`).textContent =
       scores[activePlayer - 1];
-    if (scores[activePlayer - 1] >= 10) {
+    if (scores[activePlayer - 1] >= 69) {
       playing = false;
-      // document.querySelector(".roll-dice").removeAttribute("onclick");
-      // document.querySelector(".hold").removeAttribute("onclick");
+      document.querySelector(".roll-dice").removeAttribute("onclick");
+      document.querySelector(".hold").removeAttribute("onclick");
 
-      // document.querySelector(".dice-images").style.visibi  lity = "hidden";
+      diceImage.classList.remove("show-dice-img");
 
       winningModal();
     } else {
@@ -134,36 +179,19 @@ hold.addEventListener("click", () => {
 
 // new game buttons functuality starts here!!!
 newGameBtn.addEventListener("click", () => {
-  playing = true;
-  audioNewGame.currentTime = 0;
-  activePlayer = 1;
-  currentScore = 0;
-  scores = [0, 0];
-  playerSScores();
-  playersCurrentScores();
-  // document.querySelector(".dice-images").style.visibility = "hidden";
-  diceImage.classList.remove("show-dice-img");
-
-  document.querySelector(
-    `.player${activePlayer}-current-dice-score-textContent`
-  ).textContent = Number(0);
-
-
-  setTimeout(() => {
-    player1Bg.classList.remove("unactive-color");
-    player1Bg.classList.add("active-color");
-    player2Bg.classList.add("unactive-color");
-    player2Bg.classList.remove("active-color");
-
-    player1Name.classList.add("active-player-name");
-    player1Name.classList.remove("unactive-player-name");
-    player2Name.classList.remove("active-player-name");
-    player2Name.classList.add("unactive-player-name");
-  }, 100);
+  resetGameNewGameBtn();
+  audioAddCode();
 });
 
-// $(window).on("load", function () {
-//   setTimeout(() => {
-//     $(".loader-wrapper").fadeOut("slow");
-//   }, 1000);
-// });
+// new game modal functuality starts here!!!
+modalNewGameBtn.addEventListener("click", () => {
+  resetGameNewGameBtn();
+  audioAddCode();
+})
+
+
+$(window).on("load", function () {
+  setTimeout(() => {
+    $(".loader-wrapper").fadeOut("slow");
+  }, 1400);
+});
